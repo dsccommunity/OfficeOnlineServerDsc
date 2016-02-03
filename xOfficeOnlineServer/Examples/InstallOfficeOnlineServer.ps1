92f401b7dbe1ed29ce278be9fe87db6c79a64f77
Jason Walker (MCS) <jwalker@microsoft.com>
2016-02-03 18:12:51 -0500

    Intitial push for xOffliceOnlineServerInstall

---
+++ xOfficeOnlineServer/Examples/InstallOfficeOnlineServer.ps1
@@ -0,0 +1,30 @@
+﻿Configuration InstallOOS
+{
+    Import-DscResource -ModuleName xOfficeOnlineServer
+
+    node localhost
+    {
+        WindowsFeature Web-Server
+        {
+            Name = 'Web-Server'
+            Ensure = 'Present'
+        }
+
+        WindowsFeature Web-Asp-Net45
+        {
+            Name = 'Web-Asp-Net45'
+            Ensure = 'Present'
+        }
+
+        xOfficeOnlineServerInstall Install
+        {
+            Path = 'C:\InstallBits\Setup.exe'
+            DependsOn = '[WindowsFeature]Web-Server','[WindowsFeature]Web-Asp-Net45'
+        }
+    }
+}
+
+InstallOOS -OutputPath C:\DSC
+
+Start-DscConfiguration -Path C:\DSC -Force -Wait -Verbose
+
