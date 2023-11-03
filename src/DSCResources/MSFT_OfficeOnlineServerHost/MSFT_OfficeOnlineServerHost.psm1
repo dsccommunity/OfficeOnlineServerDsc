6b6f47eaca4f5d676ca9fbfe5576e811fab11904
Jonas Cassier <jonas.cassier@mail.io>
2023-11-03 15:52:15 +0100

    Create new resource MSFT_OfficeOnlineServerHost

---
+++ src/DSCResources/MSFT_OfficeOnlineServerHost/MSFT_OfficeOnlineServerHost.psm1
@@ -0,0 +1,147 @@
+$script:resourceModulePath = Split-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -Parent
+$script:modulesFolderPath = Join-Path -Path $script:resourceModulePath -ChildPath 'Modules'
+$script:resourceHelperModulePath = Join-Path -Path $script:modulesFolderPath -ChildPath 'OfficeOnlineServerDsc.Util'
+Import-Module -Name (Join-Path -Path $script:resourceHelperModulePath -ChildPath 'OfficeOnlineServerDsc.Util.psm1')
+
+$script:localizedData = Get-LocalizedData -ResourceName (Get-Item -Path $MyInvocation.MyCommand.Path).BaseName
+
+$script:OOSDscRegKey = "HKLM:\SOFTWARE\OOSDsc"
+
+function Get-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([System.Collections.Hashtable])]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateSet('Yes')]
+        [String]
+        $IsSingleInstance,
+
+        [Parameter()]
+        [ValidateSet("Present", "Absent")]
+        [System.String]
+        $Ensure = "Present",
+
+        [Parameter()]
+        [System.String[]]
+        $AllowList
+    )
+
+    Write-Verbose -Message "Retrieving current Office Online Server Farm allow list"
+
+    Confirm-OosDscEnvironmentVariables
+
+    Import-Module -Name OfficeWebApps -ErrorAction Stop -Verbose:$false
+
+    $nullReturn = $PSBoundParameters
+    $nullReturn.Ensure = 'Absent'
+    $nullReturn.AllowList = @()
+
+    if($officeWebAppsHost = Get-OfficeWebAppsHost -ErrorAction Stop) {
+        
+        return @{
+            IsSingleInstance = 'Yes'
+            Ensure           = 'Present'
+            AllowList        = $officeWebAppsHost.AllowList.ToArray()
+        }
+    }
+    
+    return $nullReturn
+}
+
+function Set-TargetResource
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateSet('Yes')]
+        [String]
+        $IsSingleInstance,
+
+        [Parameter()]
+        [ValidateSet("Present", "Absent")]
+        [System.String]
+        $Ensure = "Present",
+
+        [Parameter()]
+        [System.String[]]
+        $AllowList
+    )
+
+    Write-Verbose -Message "Updating Office Online Server Farm allow list"
+
+    Confirm-OosDscEnvironmentVariables
+
+    Import-Module -Name OfficeWebApps -ErrorAction Stop -Verbose:$false
+
+    $CurrentValues = Get-TargetResource @PSBoundParameters
+
+    forEach($domain in $PSBoundParameters.AllowList) {
+        
+        if ($Ensure -eq 'Present' -and $domain -notin $CurrentValues.AllowList)
+        {
+            Write-Verbose -Message "Adding domain '$domain'"
+            New-OfficeWebAppsHost -Domain $domain
+        }
+        elseif ($Ensure -eq 'Absent' -and $domain -in $CurrentValues.AllowList)
+        {
+            Write-Verbose -Message "Removing domain '$domain'"
+            Remove-OfficeWebAppsHost -Domain $domain
+        }
+    }
+}
+
+function Test-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateSet('Yes')]
+        [String]
+        $IsSingleInstance,
+
+        [Parameter()]
+        [ValidateSet("Present", "Absent")]
+        [System.String]
+        $Ensure = "Present",
+
+        [Parameter()]
+        [System.String[]]
+        $AllowList
+    )
+
+    Write-Verbose -Message "Testing Office Online Server Farm allow list"
+
+    Confirm-OosDscEnvironmentVariables
+
+    $PSBoundParameters.Ensure = $Ensure
+    $CurrentValues = Get-TargetResource @PSBoundParameters
+
+    $CurrentValues.Remove('Verbose') | Out-Null
+    $PSBoundParameters.Remove('Verbose') | Out-Null
+
+    Write-Verbose -Message "Current Values: $(Convert-OosDscHashtableToString -Hashtable $CurrentValues)"
+    Write-Verbose -Message "Target Values: $(Convert-OosDscHashtableToString -Hashtable $PSBoundParameters)"
+
+  
+    forEach($domain in $PSBoundParameters.AllowList) {
+        
+        if ($Ensure -eq 'Present' -and $domain -notin $CurrentValues.AllowList)
+        {
+            return $false
+        }
+
+        if ($Ensure -eq 'Absent' -and $domain -in $CurrentValues.AllowList)
+        {
+            return $false
+        }
+    }
+
+    return $true
+}
+
+Export-ModuleMember -Function *-TargetResource
---
+++ src/DSCResources/MSFT_OfficeOnlineServerHost/en-US/MSFT_OfficeOnlineServerHost.strings.psd1
@@ -0,0 +1,3 @@
+ConvertFrom-StringData @'
+    NotApartOfAFarm = It does not appear that this machine is part of an Office Online Server/Office Web Apps farm.
+'@
