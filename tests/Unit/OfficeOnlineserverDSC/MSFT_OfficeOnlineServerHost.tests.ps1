01e3488ada0d22a247661a036c58d8d55a1130d5
Jonas Cassier <jonas.cassier@mail.io>
2023-11-09 14:18:41 +0100

    Fixes some Erorrs after branch rebase

--- tests/Unit/OfficeOnlineserverDSC/MSFT_OfficeOnlineServerHost.tests.ps1
+++ tests/Unit/OfficeOnlineserverDSC/MSFT_OfficeOnlineServerHost.tests.ps1
@@ -40,13 +40,6 @@ try
             Remove-Module -Name "OfficeWebApps" -Force -ErrorAction SilentlyContinue
             Import-Module $Global:CurrentWACCmdletModule -WarningAction SilentlyContinue
 
-            # # Mock OfficeWebAppsHost .net object
-            # $OfficeWebAppsHostObject = [scriptblock]::Create({
-            #         New-Object -TypeName PSCustomObject -Property @{
-            #             allowList = [System.Collections.Generic.List`1[[System.String, mscorlib, Version = 4.0.0.0, Culture = neutral, PublicKeyToken = b77a5c561934e089]]]::new()
-            #         }
-            #     })
-
             Mock -CommandName Get-OfficeWebAppsHost -MockWith {}
             Mock -CommandName Remove-OfficeWebAppsHost -MockWith {}
             Mock -CommandName New-OfficeWebAppsHost -MockWith {}
@@ -155,7 +148,6 @@ try
                     $instance = [PSCustomObject] @{
                         allowList = [System.Collections.Generic.List`1[[System.String, mscorlib, Version = 4.0.0.0, Culture = neutral, PublicKeyToken = b77a5c561934e089]]]::new()
                     }
-                    <<<<<<< HEAD
                     $instance.allowList.Add("oos2.contoso.com")
                     return $instance
                 }
@@ -182,11 +174,9 @@ try
                     }
                     $instance.allowList.Add("oos1.contoso.com")
                     $instance.allowList.Add("oos2.contoso.com")
-                    =======
                     $instance.allowList.Add("oos1.contoso.com")
                     $instance.allowList.Add("oos2.contoso.com")
                     $instance.allowList.Add("oos3.contoso.com")
-                    >>>>>>> 985cbbf (First draft for unit tests completed)
                     return $instance
                 }
 
@@ -211,11 +201,8 @@ try
                     $instance = [PSCustomObject] @{
                         allowList = [System.Collections.Generic.List`1[[System.String, mscorlib, Version = 4.0.0.0, Culture = neutral, PublicKeyToken = b77a5c561934e089]]]::new()
                     }
-                    <<<<<<< HEAD
                     $instance.allowList.Add("oos1.contoso.com")
-                    =======
                     $instance.allowList.Add("oos2.contoso.com")
-                    >>>>>>> 985cbbf (First draft for unit tests completed)
                     return $instance
                 }
 
